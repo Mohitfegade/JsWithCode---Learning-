10af1cab75514b1fd72f698585e81623fb069503
Mohitfegade <mohitfegade777@gmail.com>
2026-08-25 07:31:17 +0000

    learned variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId =12345 //cant change const 
+let accountEmail ="mohitfegade777@gmail.com" // can change  not global scope 
+var accountPassword ="12345" // global scope & can change avoid using var
+
+// console.log se ek variable print ho sakta hai but console.table use krke dekho tabel format mai sab variable print honge
+console.table([accountId,accountEmail,accountPassword])
